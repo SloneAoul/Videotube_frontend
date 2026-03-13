0afbd534375c59e3fd65d4866f2e1a7d4bc803d3
SloneAoul <shorya16705@gmail.com>
2026-03-13 19:27:04 +0530

    update backend url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: "https://videotube-backend-s8ks.onrender.com/api/v1",
     withCredentials: true  // important for cookies/JWT
 })
 
